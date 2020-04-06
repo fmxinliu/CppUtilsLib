87b6f23d5292d93720d3788bc12b426564c5b171
xin liu <xinliuchnsz@gmail.com>
2020-04-06 17:23:06 +0800

    更新打包脚本

--- build.bat
+++ build.bat
@@ -1,26 +1,26 @@
 @echo off
 call "%VS100COMNTOOLS%vsvars32.bat"
 
-if exist "deploy" (
-    rmdir deploy /q/s
-)
+if exist deploy  rmdir deploy /q /s
 
 cd Leakpress
 
 :MSBuild /t:Clean;Rebuild /p:Configuration=Release /m
 msbuild -t:clean -p:configuration="release" -m -t:rebuild
+@if not "%errorlevel%"=="0" goto error
 
 cd ..
 mkdir deploy
-if exist "deploy" (
-    copy Leakpress\Release\Leakpress.exe deploy\Leakpress.exe
-    copy Leakpress\Release\MfcDllTool.dll deploy\MfcDllTool.dll
-    xcopy Leakpress\Release\*.pdb deploy\*.pdb
-    xcopy Leakpress\Leakpress\Config\*.ini deploy\*.ini
-    xcopy Leakpress\Leakpress\DllTool\dll\*.dll deploy\*.dll
-    xcopy Leakpress\Leakpress\DllTool\dll\VC100.CRT\release\*.dll deploy\*.dll
-)
 
-@echo on
+xcopy Leakpress\Release\*.pdb deploy\*.pdb                                  /C /I /R /K /Y
+xcopy Leakpress\Leakpress\Config\*.ini deploy\*.ini                         /C /I /R /K /Y
+xcopy Leakpress\Leakpress\DllTool\dll\*.dll deploy\*.dll                    /C /I /R /K /Y
+xcopy Leakpress\Leakpress\DllTool\dll\VC100.CRT\release\*.dll deploy\*.dll  /C /I /R /K /Y
+ copy Leakpress\Release\Leakpress.exe deploy\Leakpress.exe                  /Y
+@if "%errorlevel%"=="0" goto end
 
+:error
+@echo === build fail ===
+
+:end
 pause
