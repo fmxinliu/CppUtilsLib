0bc131439dc940ce74452f6e441494540d8f04de
xin liu <xinliuchnsz@gmail.com>
2020-05-11 23:53:33 +0800

    添加注册表操作类RegistryHelper

---
+++ MfcDllTool/utiltools/include/RegistryHelper.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "dllexport.h"
+
+namespace UtilTools
+{
+    class DLL_API RegistryHelper
+    {
+    public:
+        //---------------------------------------------------------------
+        //function: 
+        //     GetRegValue 获取注册表中指定键的值
+        //Access:
+        //      public 
+        //Parameter:
+        //     [in] int nKeyType - 注册表项的类型，传入的参数只可能是以下数值：
+        //               0:HKEY_CLASSES_ROOT
+        //               1:HKEY_CURRENT_USER
+        //               2:HKEY_LOCAL_MACHINE
+        //               3:HKEY_USERS
+        //               4:HKEY_PERFORMANCE_DATA
+        //               5:HKEY_CURRENT_CONFIG
+        //               6:HKEY_DYN_DATA
+        //               7:HKEY_CURRENT_USER_LOCAL_SETTINGS
+        //               8:HKEY_PERFORMANCE_TEXT
+        //               9:HKEY_PERFORMANCE_NLSTEXT
+        //     [in] const std::string & strUrl - 要查找 的键的路径
+        //     [in] const std::string & strKey - 指定的键
+        //Returns:
+        //     String - 指定键的值
+        //Remarks:
+        //     ...
+        //---------------------------------------------------------------
+        static String GetRegValue(int nKeyType, const String& strUrl, const String& strKey, bool bWow64_32KEY = true);
+    };
+}
+
